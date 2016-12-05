4d18b16bc44dcd4495648b40e8718529f7991afb
Administrator <Administrator@7A304-0036>
2016-12-05 17:38:42 +0800

    补

--- src/com/example/fragment/PictureInputCellFragment.java
+++ src/com/example/fragment/PictureInputCellFragment.java
@@ -2,8 +2,15 @@ package com.example.fragment;
 
 import com.example.helloworld.R;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,7 +19,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class PictureInputCellFragment extends Fragment {
-
+final int REQUEST_CAMERA=1;
+final int REQUEST_ALBUM=2;
 ImageView image;
 TextView label;
 TextView hint;
@@ -22,8 +30,60 @@ public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle sa
 	label=(TextView)view.findViewById(R.id.label);
 	hint=(TextView)view.findViewById(R.id.hint);
 	image=(ImageView)view.findViewById(R.id.image);
-	
+	image.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			String[] items=new String[]{
+				"≈ƒ’’","œ‡≤·"
+			};
+			new AlertDialog.Builder(getActivity())
+			.setTitle(label.getText())
+			.setItems(items,new android.content.DialogInterface.OnClickListener(){
+				
+				public void onClick(DialogInterface dialog, int which) {
+					// TODO Auto-generated method stub
+					switch (which) {
+					case 0:
+						takePhoto();
+						break;
+					case 1:
+						pickFromAlbum();
+					default:
+						break;
+					}
+				}
+			})
+			.setNegativeButton("»°œ˚", null)
+			.show();
+		}
+		void takePhoto(){
+			Intent itnt=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			startActivityForResult(itnt, REQUEST_CAMERA);
+		}
+		void pickFromAlbum(){
+			Intent itnt=new Intent(Intent.ACTION_GET_CONTENT);
+			itnt.setType("image/*");
+			startActivityForResult(itnt, REQUEST_ALBUM);
+		}
+	});
 	return view;
+}
+public void onActivtyForResult(int requestCode,int resultCode,Intent data){
+	if(requestCode==Activity.RESULT_CANCELED)return;
+	if(requestCode==REQUEST_CAMERA){
+		Bitmap bmp=(Bitmap)data.getExtras().get("data");
+		image.setImageBitmap(bmp);
+	}else if(requestCode==REQUEST_ALBUM){
+		try{
+			Bitmap bmp = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), data.getData());
+			image.setImageBitmap(bmp);
+		}catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
 }
 public void setLabelText(String labelText){
 	this.label.setText(labelText);
